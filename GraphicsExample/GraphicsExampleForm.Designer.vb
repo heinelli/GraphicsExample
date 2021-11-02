ef9b7672782ea28d06259a7363c1d7e14a7914c6
Elliot Heiner <elliotheiner1997@gmail.com>
2021-11-02 08:40:15 -0600

    Add code to draw a line and a circle

--- GraphicsExample/GraphicsExampleForm.Designer.vb
+++ GraphicsExample/GraphicsExampleForm.Designer.vb
@@ -22,10 +22,30 @@ Partial Class GraphicsExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Location = New System.Drawing.Point(28, 24)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(1031, 670)
+        Me.PictureBox.TabIndex = 0
+        Me.PictureBox.TabStop = False
+        '
+        'GraphicsExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1091, 727)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Name = "GraphicsExampleForm"
         Me.Text = "Form1"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PictureBox As PictureBox
 End Class
